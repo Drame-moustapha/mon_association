94008b82f055217cc855f274d6661a6fb8e17c6f
smd <160590672+Drame-moustapha@users.noreply.github.com>
2026-07-09 01:45:31 +0000

    frontend: update app module with new components and routing

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,22 +5,30 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login.component';
+import { HomeComponent } from './home.component';
+import { NavbarComponent } from './navbar.component';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AuthService } from './services/auth.service';
+import { ActivityService } from './services/activity.service';
 import { TokenInterceptor } from './services/token.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     AuthService,
+    ActivityService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: TokenInterceptor,
